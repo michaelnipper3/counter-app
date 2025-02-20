6398e76e6b1627e7cd227ce99427e149587d30a1
michaelnipper3 <118383611+michaelnipper3@users.noreply.github.com>
2025-02-20 13:20:28 -0500

    added most functionality including all constructors

--- counter-app.js
+++ counter-app.js
@@ -32,7 +32,11 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         new URL("./locales/counter-app.ar.json", import.meta.url).href +
         "/../",
       locales: ["ar", "es", "hi", "zh"],
+      
     });
+    this.counter = 0;
+    this.min = -100;
+    this.max = 100;
   }
 
   // Lit reactive properties
@@ -40,9 +44,41 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      counter: { type: Number, reflect: true },
+      max: { type: Number },
+      min: { type: Number }
     };
   }
 
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    if (changedProperties.has('counter')) {
+      if (this.counter === 21){
+        this.makeItRain();
+      }
+    }
+  }
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
   // Lit scoped styles
   static get styles() {
     return [super.styles,
@@ -53,23 +89,43 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
+      :host([counter="18"]) {
+        color: var(---ddd-theme-default-athertonViolet);
+      }
       .wrapper {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
       }
-      h3 span {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+      .counter{
+        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xxl));
       }
     `];
+  }
+
+  decrease(){
+    if ((this.counter-1) >= this.min) {
+      this.counter = this.counter-1
+    }
+  }
+
+  increase(){
+    if ((this.counter+1) <= this.max) {
+      this.counter = this.counter+1
+    }
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <confetti-container id="confetti">
+      <div class="wrapper">
+      <div class="counter">${this.counter}</div>
+      <div class="buttons">
+        <button @click="${this.decrease}" ?disabled="${this.min === this.counter}">-1</button>
+        <button @click="${this.increase}" ?disabled="${this.max === this.counter}">+1</button>
+      </div>
+      </div>
+      </confetti-container>;`
   }
 
   /**
